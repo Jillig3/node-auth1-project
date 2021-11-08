5d467f6668d938215304cebea0511d0e55ec4504
jillig3 <jacobillig@gmail.com>
2021-11-08 16:19:03 -0600

    server js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router');
+const session = require("express-session");
+
+session = require('express-session')
+const Store = require('connect-session-knex')(session)
+
+const knex = require('../data/db-config')
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -16,14 +24,42 @@ const cors = require("cors");
  */
 
 const server = express();
+server.use(express.json());
+
+server.use(session({
+  name: 'totallygenericname',
+  secret: 'nevertell',
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,
+    createTable: true,
+    clearInterval: 1000 * 60 * 10,
+    tablename: 'sessions',
+    sidfieldname: 'sid'
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true
+  }
+}))
+
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
 
 server.use(helmet());
-server.use(express.json());
 server.use(cors());
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
+
+server.get('*', (req, res) => {
+  res.status(404).json({
+    message: 'Not Found'
+  })
+})
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
