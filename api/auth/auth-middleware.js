e40eb0ac8cfc74176faa7e5f5423f90efe2fecf3
jillig3 <jacobillig@gmail.com>
2021-11-08 16:25:15 -0600

    users router & middleware fix

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -12,7 +12,7 @@ function restricted(req, res, next) {
   if (req.session.user) {
     next()
   } else {
-    next({ status: 401, message: 'Get outta here!'})
+    next({ status: 401, message: 'You shall not pass!'})
   }
 }
 
